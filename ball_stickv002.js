5ee6b672c677d9b58fcb5111ec0acd04d10e8865
Chris Nicoll <zeta@cnicoll.net>
2015-06-02 12:08:29 -0400

    Basic code to draw a stick attached to a circle
    First commit

---
+++ ball_stickv002.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function initImage(){
+    // This function is to deal with generating positions for the circle-stick entity
+    // (element) with id elemId
+    // Start with hard-coded values; expect them to come from pointer input later
+    var circlex = 30, 
+    circley = 30,
+    circlerad = 5,
+    stickstartx = circlex,
+    stickstarty = circley + 1.5*circlerad,
+    stickendx = stickstartx,
+    stickendy = stickstarty + 10*circlerad;
+
+   /* function genPosns(){
+        this.circlex = circlex;
+        this.circley = circley;
+        this.circlerad = circlerad;
+        this.stickstartx = stickstartx;
+        this.stickstarty = stickstarty;
+        this.stickendx = stickendx;
+        this.stickendy = stickendy;
+    }*/
+
+    //function draw(){
+    var canvas = document.getElementById('main_canvas');
+
+    if (canvas.getContext){
+        var ctx = canvas.getContext('2d');
+
+        //Draw a circle to make sure everything's not broken
+        ctx.beginPath();
+        ctx.arc(100, 100, 12, 0, 2*Math.PI);
+        ctx.stroke();
+
+        //Draw the circle
+        ctx.beginPath();
+        ctx.arc(elem.circlex, elem.circley, elem.circlerad, 0, 2*Math.PI);
+        ctx.stroke();
+
+        // Draw the stick
+        ctx.beginPath();
+        ctx.moveTo(elem.stickstartx, elem.stickstarty);
+        ctx.lineTo(elem.stickendx, elem.stickendy);
+        ctx.stroke();
+    }
+    else {
+        //canvas not supported so do some kind of fallback
+    }
+    //}
+
+    //draw();
+}
+initImage();
